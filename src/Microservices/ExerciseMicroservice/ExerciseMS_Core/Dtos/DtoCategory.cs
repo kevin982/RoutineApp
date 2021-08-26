72280b146cc629f1ae8498ca49a38c21ebdf20c4
Kevin Andres Vasquez Benavides <kevinavbdev@outlook.com>
2021-08-26 10:59:54 -0600

    Dto Category has been added.

---
+++ src/Microservices/ExerciseMicroservice/ExerciseMS_Core/Dtos/DtoCategory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseMS_Core.Dtos
+{
+    public class DtoCategory
+    {
+        public Guid CategoryId { get; set; }
+
+        public string CategoryName { get; set; }
+    }
+}
