89e86a434a6ee7ae0eb8bdcea2afd1519ae005ac
Kevin Andres Vasquez Benavides <kevinavbdev@outlook.com>
2021-08-26 10:57:17 -0600

    The services have been declared.

---
+++ src/Microservices/ExerciseMicroservice/ExerciseMS_Core/Services/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseMS_Core.Services
+{
+    public interface IUserService
+    {
+        Task<string> GetUserId();
+    }
+}
