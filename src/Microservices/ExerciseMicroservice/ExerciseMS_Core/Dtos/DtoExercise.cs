f598798e81334abddafb1afbcf1ed1d0cc460430
Kevin Andres Vasquez Benavides <kevinavbdev@outlook.com>
2021-08-26 10:13:18 -0600

    Dtos have been added

---
+++ src/Microservices/ExerciseMicroservice/ExerciseMS_Core/Dtos/DtoExercise.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseMS_Core.Dtos
+{
+    public class DtoExercise
+    {
+        public Guid Id { get; set; }
+
+        public string ExerciseName { get; set; }
+
+        public string CategoryName { get; set; }
+
+        public bool IsInTheRoutine { get; set; }
+
+        public string ImageUrl { get; set; }
+    }
+}
