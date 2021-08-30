055c1324a2580ee596d1aa626ae962942e60f51d
Kevin Andres Vasquez Benavides <kevinavbdev@outlook.com>
2021-08-30 10:07:09 -0600

    The commands have been added.

---
+++ src/Microservices/ExerciseMicroservice/ExerciseMS_Application/Commands/CreateCategoryCommand.cs
@@ -0,0 +1,20 @@
+﻿using ExerciseMS_Core.Models.Requests;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseMS_Application.Commands
+{
+    public class CreateCategoryCommand : IRequest<bool>
+    {
+        public CreateCategoryRequest CreateCategoryRequest { get; set; }
+
+        public CreateCategoryCommand(CreateCategoryRequest createCategoryRequest)
+        {
+            CreateCategoryRequest = createCategoryRequest;
+        }
+    }
+}
---
+++ src/Microservices/ExerciseMicroservice/ExerciseMS_Application/Commands/DeleteExerciseCommand.cs
@@ -0,0 +1,19 @@
+﻿using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseMS_Application.Commands
+{
+    public class DeleteExerciseCommand : IRequest<bool>
+    {
+        public Guid ExerciseId { get; init; }
+
+        public DeleteExerciseCommand(Guid exerciseId)
+        {
+            ExerciseId = exerciseId;
+        }
+    }
+}
