1f6b3edd3c8fce876b3bae04af2ac5f0d70b36da
Kevin Andres Vasquez Benavides <kevinavbdev@outlook.com>
2021-08-30 09:04:52 -0600

    We have declared and implemented the UoW.

---
+++ src/Microservices/ExerciseMicroservice/ExerciseMS_Core/UoW/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using ExerciseMS_Core.Repositories;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseMS_Core.UoW
+{
+    public interface IUnitOfWork
+    {
+        IExerciseRepository Exercises { get; }
+        ICategoryRepository Categories { get; }
+        Task CompleteAsync();
+    }
+}
---
+++ src/Microservices/ExerciseMicroservice/ExerciseMS_Infraestructure/UoW/UnitOfWork.cs
@@ -0,0 +1,45 @@
+﻿using ExerciseMS_Core.Repositories;
+using ExerciseMS_Core.UoW;
+using ExerciseMS_Infraestructure.Data;
+using ExerciseMS_Infraestructure.Repositories;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExerciseMS_Infraestructure.UoW
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly ExerciseMsDbContext _dbContext;
+
+        private readonly ILogger<UnitOfWork> _logger;
+
+        public IExerciseRepository Exercises { get; private set; }
+
+        public ICategoryRepository Categories { get; private set; }
+
+        public UnitOfWork(ExerciseMsDbContext dbContext, ILogger<UnitOfWork> logger)
+        {
+            _dbContext = dbContext;
+            
+            Exercises = new ExerciseRepository(_dbContext);
+            Categories = new CategoryRepository(_dbContext);
+        }
+
+        public async Task CompleteAsync()
+        {
+            try
+            {
+                await _dbContext
+                    .SaveChangesAsync();
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error while saving changes because of: {ex.Message}");
+            }
+        }
+    }
+}
