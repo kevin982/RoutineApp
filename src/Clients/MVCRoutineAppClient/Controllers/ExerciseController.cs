b59a23e660a0041fdad8735217a88569b76ca461
Kevin Andres Vasquez Benavides <kevinavbdev@outlook.com>
2021-09-14 17:29:11 -0600

    Create exercise view.

---
+++ src/Clients/MVCRoutineAppClient/Controllers/ExerciseController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using MVCRoutineAppClient.Filters;
+using MVCRoutineAppClient.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCRoutineAppClient.Controllers
+{
+    [ApiController]
+    [Authorize]
+    public class ExerciseController : Controller
+    {
+        [UserAuthorizationFilter]
+        [HttpGet("/v1/Exercise")]
+        public IActionResult CreateExercise()
+        {
+            return View();
+        }
+
+        [UserAuthorizationFilter]
+        [HttpPost("/v1/Exercise")]
+        public IActionResult CreateExercise(CreateExerciseRequestModel model)
+        {
+            //I must implement the logic to add the exercise here.
+
+            return View();
+        }
+
+        [AdminAuthorizationFilter]
+        [HttpGet("/v1/Category")]
+        public IActionResult CreateCategory()
+        {
+            return View();
+        }
+
+        [AdminAuthorizationFilter]
+        [HttpPost("/v1/Category")]
+        public IActionResult CreateCategory([FromForm]CreateCategoryRequestModel model)
+        {
+            //I must implement the logic to add the category here.
+
+            return View();
+        }
+
+        [UserAuthorizationFilter]
+        [HttpGet("/v1/Categories")]
+        public async Task<ActionResult<string>> GetAllCategories()
+        {
+            //I must implement the logic to get all the categories here
+
+            return "";
+        }
+
+    }
+}
---
+++ src/Clients/MVCRoutineAppClient/Models/CreateExerciseRequestModel.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCRoutineAppClient.Models
+{
+    public class CreateExerciseRequestModel
+    {
+        [Required(ErrorMessage = "You must enter the exercise name"), Display(Name = "Please enter the exercise name"), MaxLength(30)]
+        public string Name { get; set; } = string.Empty;
+
+        [Display(Name = "Please choose the category")]
+        public string Category { get; set; } = string.Empty;
+
+        [Display(Name = "Choose the exercise image"), Required(ErrorMessage = "You must choose an image")]
+        public IFormFile Image { get; set; }
+        public string ImageUrl { get; set; } = string.Empty;
+    }
+}
