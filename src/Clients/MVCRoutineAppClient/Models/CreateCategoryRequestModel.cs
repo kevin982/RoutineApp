f728ddf0bdd34bb831adcfd8a6be4065afa768a3
Kevin Andres Vasquez Benavides <kevinavbdev@outlook.com>
2021-09-14 17:29:01 -0600

    Create category view created.

---
+++ src/Clients/MVCRoutineAppClient/Models/CreateCategoryRequestModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVCRoutineAppClient.Models
+{
+    public class CreateCategoryRequestModel
+    {
+        [Required(ErrorMessage = "Please provide the category name"), MinLength(3), MaxLength(30)]
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}
